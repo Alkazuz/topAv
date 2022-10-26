25605682f7729d427b1a3d5923d7fd75e06f7307
CintiaLara <>
2022-10-26 19:38:39 -0400

    Correção

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -1,9 +1,52 @@
 import { Request, Response } from 'express';
 const Users = require('../users.json');
 
-/**
- * Use o conteúdo da variável `Users` para desenvolver os métodos necessários
- */
+interface Usuario{
+    id:number;
+    nome:String;
+    sobrenome:String;
+    email:String;
+    sexo:String;
+    idade:number;
+}
+
 export default {
-    // o conteúdo do controller vai aqui
-};
+        //listar
+        async listar(request: Request, response: Response) {
+            const usuario = Users;
+            if(usuario){
+                return response.status(200).json(usuario);
+            }
+            return response.status(400).json({ message: 'nenhum usuário foi encontrado!' });
+        },
+
+        async buscarId(request : Request, response : Response){
+            const {id} = request.params;
+            const usuario = Users.find((usuario: Usuario)=>usuario.id===parseInt(id));
+            if(usuario){
+                return response.status(200).json(usuario);
+            }
+            return response.status(400).json({ message: 'usuario nao encontrado, confira o "iD" inserido' });
+            
+        },
+
+        async buscarIdade(request : Request, response : Response){
+            const {idade} = request.body;
+            const resultado = Users.filter((usuario: Usuario)=>
+                usuario.idade > idade);
+            if(resultado){
+                return response.status(200).json(resultado);
+            }
+            return response.status(400).json({message : 'usuario não encontrado, confira a idade inserida!'})
+        },
+
+        async buscarSexo(request : Request, response : Response){
+            const {sexo} = request.body;
+            const resultado = Users.filter((usuario:Usuario)=>
+                usuario.sexo == sexo);
+            if(resultado){
+                return response.status(200).json(resultado);
+            }
+            return response.status(400).json({message: 'usuario não encontrado, confira o sexo inserido!'})
+        },        
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,20 @@
 import express from 'express';
 import UserController from '../controllers/UserControllers';
 const routes = express.Router();
-
-routes.get('/list' /**  */);
-// continuar aqui
+routes.get('/usuario/listar',UserController.listar);
+routes.get("/usuario/:id",UserController.buscarId);
+routes.post("/usuario/idade",UserController.buscarIdade);
+routes.post("/usuario/sexo",UserController.buscarSexo);
 export default routes;
+
+/*import express from 'express';
+import produtoController from '../controllers/userControlerProduto';
+const produtoRoutes = express.Router();
+
+produtoRoutes.post('/new', produtoController.create);
+produtoRoutes.put('/update', produtoController.update);
+produtoRoutes.get('/list', produtoController.index);
+produtoRoutes.post('/find', produtoController.findOne);
+produtoRoutes.post('/delete', produtoController.delete);
+
+export default produtoRoutes;
